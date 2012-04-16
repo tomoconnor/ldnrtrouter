85ce330966075d233b91960029927b5e57ed23ea
Tom O'Connor <tom.oconnor@baseblack.com>
2012-04-16 11:58:24 +0100

    adding the config for the proxy

---
+++ proxy/localproxy.js
@@ -0,0 +1,3 @@
+var mpjegproxy = require("./node-mjpeg-proxy");
+mpjegproxy.createProxy("http://192.168.6.6/mjpg/video.mjpg");
+
---
+++ proxy/remoteproxy.js
@@ -0,0 +1,3 @@
+var mpjegproxy = require("./node-mjpeg-proxy");
+mpjegproxy.createProxy("http://172.16.32.200:5080/");
+
